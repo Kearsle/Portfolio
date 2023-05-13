adbd66f69aff2c59fe94d3eaf9556033ed313697
Kearsle <danielkearsleybrown@gmail.com>
2023-05-13 15:42:40 +0100

    Nav Bar Bugfix

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,7 @@ function Navbar() {
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          <Link to="/portfolio" className="navbar-logo" onClick={closeMobileMenu}>
             Daniel Kearsley-Brown
           </Link>
           <div className="menu-icon" onClick={handleClick}>
